4f79ffb1fc215aa78002d50c4c6e4013168a5b61
JordAnne <jbethune37@gmail.com>
2023-05-25 21:45:40 -0400

    updated controllers and routes. still need to fix

--- controllers/thoughtsController.js
+++ controllers/thoughtsController.js
@@ -28,12 +28,81 @@ module.exports = {
         console.log(err);
         return res.status(500).json(err);
       });
-  }
+  },
 
 
 
+//more to add for more functions
 
 
 
+  // Update a thought
+updateThought(req, res) {
+  Thought.findOneAndUpdate(
+    { _id: req.params.thoughtId },
+    { $set: req.body },
+    { runValidators: true, new: true }
+  )
+    .then((thought) =>
+      !thought
+        ? res.status(404).json({ message: 'No thought with this id!' })
+        : res.json(thought)
+    )
+    .catch((err) => res.status(500).json(err));
+},
+
+// Delete a thought
+deleteThought(req, res) {
+  Thought.findOneAndDelete({ _id: req.params.thoughtId })
+    .then((thought) =>
+      !thought
+        ? res.status(404).json({ message: 'No thought with that ID' })
+        : Reaction.deleteMany({ _id: { $in: thought.reactions } })
+    )
+    .then(() => res.json({ message: 'Thought and Reactions Deleted!' }))
+    .catch((err) => res.status(500).json(err));
+},
+
+// Add a reaction
+createReaction(req, res) {
+Thought.findOneAndUpdate(
+  { _id: req.params.thoughtId },
+  { $addToSet: { reactions: req.body } },
+  { runValidators: true, new: true }
+)
+  .then((thought) =>
+    !thought
+      ? res.status(404).json({ message: 'No thought with that ID' })
+      : res.json(thought)
+  )
+  .catch((err) => {
+    console.log(err);
+    res.status(500).json(err);
+  });
+},
+
+// Remove a reaction
+deleteReaction(req, res) {
+Thought.findOneAndUpdate(
+  { _id: req.params.thoughtId },
+  { $pull: { reactions: { reactionId: req.params.reactionId } } },
+  { runValidators: true, new: true }
+)
+  .then((thought) =>
+    !thought
+      ? res.status(404).json({ message: 'No thought with that ID' })
+      : res.json(thought)
+  )
+  .catch((err) => {
+    console.log(err);
+    res.status(500).json(err);
+  });
+},
 
 }
+
+
+
+
+
+
